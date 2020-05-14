05dece5cbb1dfcdf80af8fbe896c0926648559cf
Adrian Romero <radrian031613@gmail.com>
2020-05-13 21:07:38 -0700

    set up Link props from react-router-dom to set up front end pagination

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,28 @@
+//turning into class based component
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+
+class App extends Component {
+  render () {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 700 }}>
+          {/* build a navbar to navigate through pages */}
+
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default App;
+
+
+// import React from 'react';
+
+// export default (props) => (
+//   <div>
+//     I am the app component!!!
+//     { props.children }
+//   </div>
+// );
