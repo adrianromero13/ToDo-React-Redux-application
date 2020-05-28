61832106bb7bb33af2130626903359d68c2dcf6a
Adrian Romero <radrian031613@gmail.com>
2020-05-27 21:07:31 -0700

    create env variables for production

--- client/src/containers/UserTodoList/index.js
+++ client/src/containers/UserTodoList/index.js
@@ -1,48 +1,37 @@
 import React, { Component } from 'react';
-import { reduxForm, Field, formValueSelector } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { Header, Form, Segment, Message, List, Pagination, Button } from 'semantic-ui-react';
 import { compose } from 'redux';
 import axios from 'axios';
-
 import UserTodoListItems from './UserTodoListItems';
-import requireAuth from './../../hoc/requireAuth';
-
-import { getUserTodos, updateTodoCompletedById, deleteTodoById } from './../../actions/todos';
-import { ADD_TODO_ERROR, ADD_TODO } from './../../actions/types';
-
-
+import requireAuth from '../../hoc/requireAuth';
+import { getUserTodos, updateTodoCompletedById, deleteTodoById } from '../../actions/todos';
+import { ADD_TODO_ERROR, ADD_TODO } from '../../actions/types';
 class UserTodoList extends Component {
-
   state = {
     activePage: 1,
     start: 0,
     end: 10
   }
-
-
   onSubmit = async (formValues, dispatch) => {
     try {
-      // formvalues = { text: 'whatever' } = req.body.text
-      //post request takes 3 params (link, what parameters you want to send to server, values (in this case a token for authentication))
-      await axios.post('/api/user/todos', formValues, { headers: { 'authorization': localStorage.getItem('token') } });
+      await axios.post('/api/user/todos', formValues, { headers: { 'authorization': localStorage.getItem('token')}} );
       dispatch({ type: ADD_TODO });
       this.props.getUserTodos();
     } catch (e) {
-      ;
+      dispatch({ type: ADD_TODO_ERROR, payload: e });
     }
   }
-
   componentDidMount() {
     this.props.getUserTodos();
   }
-
   renderAddTodo = ({ input, meta }) => {
     return (
       <>
         <Form.Input
           {...input}
-          error={meta.touched && meta.error}
+          error={ meta.touched && meta.error }
           fluid
           autoComplete='off'
           placeholder='Add a todo'
@@ -50,56 +39,45 @@ class UserTodoList extends Component {
       </>
     )
   }
-
-  //function for state dealing with pagination
   handlePageChange = (event, data) => {
-    console.log(data);
     this.setState({
       activePage: data.activePage,
       start: data.activePage === 1 ? 0 : data.activePage * 10 - 10,
       end: data.activePage * 10
     });
   }
-
   render() {
     const { handleSubmit } = this.props;
+    console.log(this.props);
     return (
-      //parent tag
       <>
-        <Header as='h2' color='teal' textAlign='center' content='Welcome to the todo app' />
+        <Header as='h2' color='teal' textAlign='center' content='Welcome to do the todo app'/>
         <Form size='large' onSubmit={handleSubmit(this.onSubmit)}>
           <Segment stacked>
             <Field
               name='text'
-              component={this.renderAddTodo} //declare prior to render
-
+              component={this.renderAddTodo}
             />
             <Button
-              // this will just use the form's onSubmit function when submit button is clicked
               type='submit'
               fluid
               color='teal'
-              content='Add a Todo'
+              content='Add a todo'
             />
           </Segment>
         </Form>
         <List animated divided selection>
-          {/* adding slice with state to create pagination */}
-          <UserTodoListItems 
-            todos={this.props.todos.slice(this.state.start, this.state.end)} 
+          <UserTodoListItems
+            todos={this.props.todos.slice(this.state.start, this.state.end)}
             handleUpdate={this.props.updateTodoCompletedById}
-            handleDelete={this.props.deleteTodoById}
-          /> {/* this.props.todos uptained from HOC from mapStateToProps and pass in to this container being rendered*/}
-
+            handleDelete={this.props.deleteTodoById}/>
         </List>
         {
-          // if length is equal to 0 then do not render pagination
           this.props.todos.length <= 9 ?
             null
             : <Pagination
-              totalPages={Math.ceil(this.props.todos.length / 10)}
-              onPageChange={(event, data) => this.handlePageChange(event, data)}
-              // activePage prop lets know what active page it is
+              totalPages={ Math.ceil( this.props.todos.length / 10) }
+              onPageChange={ (event, data) =>  this.handlePageChange(event, data) }
               activePage={this.state.activePage}
             />
         }
@@ -107,17 +85,14 @@ class UserTodoList extends Component {
     );
   }
 }
-
-// function matStateToProps(state) {
+// function mapStateToProps(state) {
 //   return {
 //     todos: state.todos.userTodos,
 //     clientError: state.todos.getUserTodosClientError,
 //     serverError: state.todos.getUserTodosServerError
 //   };
 // }
-
-// 
-function mapStateToProps({ todos: { userTodos, getUserTodosServerError, getUserTodosClientError, deleteTodoByIdError } }) {
+function mapStateToProps({ todos: { userTodos, getUserTodosServerError, getUserTodosClientError, deleteTodoByIdError}}) {
   return {
     todos: userTodos,
     clientError: getUserTodosClientError,
@@ -125,18 +100,14 @@ function mapStateToProps({ todos: { userTodos, getUserTodosServerError, getUserT
     deleteTodoByIdError,
   };
 }
-
-// 1st way
+// const composedComponent = connect(mapStateToProps, { getUserTodos })(UserTodoList);
+// 1 way
 // export default reduxForm({ form: 'addTodo' })(connect(mapStateToProps, { getUserTodos })(UserTodoList));
-
 // 2nd way
 // const composedComponent = connect(mapStateToProps, { getUserTodos })(UserTodoList);
 // export default reduxForm({ form: 'addTodo'})(composedComponent);
-
-// 3rd way
 export default compose(
   reduxForm({ form: 'addTodo' }),
   requireAuth,
-  // make sure your connect function has all the components 
   connect(mapStateToProps, { getUserTodos, updateTodoCompletedById, deleteTodoById })
 )(UserTodoList);
